303ab27c0ca55183cf23cccf7b0c12ed25f8d31a
Jack Robbins <jackrobbins1@gmail.com>
2019-03-19 21:04:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ def line(arr)
   if arr == []
     puts "The line is currently empty."
   else
-    statement = ""
-    print "The line is currently: "
+    statement = "The line is currently: "
+    
     arr.each_with_index do |el, index|
       print "#{index+1}. #{el} "
     end
