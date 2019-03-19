bcbeba4fc9ed083cfea88e4df031d1f0489e1370
Jack Robbins <jackrobbins1@gmail.com>
2019-03-19 20:52:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 def line(arr)
   if arr == []
     puts "The line is currently empty."
+  else
+    print "The line is currently:"
+    arr.each_with_index do |el, index|
+      print
+    end
   end
 end
